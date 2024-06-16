87ee09942f29f6d3bca65b8015140d0d643ea60a
Andrew Vassilyev <xavescor@xavescor.dev>
2024-06-17 00:14:54 +0500

    reexport missing elements

--- src/index.tsx
+++ src/index.tsx
@@ -3,3 +3,5 @@ export { defaults } from "./defaults";
 export { rest } from "./rest";
 export { wireHook } from "./wireHook";
 export { getAllPropsSignal } from "./getAllPropsSignal";
+export { wrap } from "./wrap";
+export { html } from "./html";
